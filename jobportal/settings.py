528036393a634b96e64c53175aedae3c73e55efc
Prahlad-Shakyaa <prahladshakya2058@gmail.com>
2024-12-31 12:27:21 +0545

    sql port fixed

--- jobportal/settings.py
+++ jobportal/settings.py
@@ -125,8 +125,8 @@ DATABASES = {
 
 # sending emails
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'prahladshakya6954@gmail.com'
-EMAIL_HOST_PASSWORD = 'uwmytuwortycfabm'
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')    
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
